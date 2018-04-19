969f4ad105679ec463b789df39bb70e95b94e211
Jesse <jessebwalsh@gmail.com>
2018-04-19 14:57:49 +1200

    2

--- upload.php
+++ upload.php
@@ -1,6 +1,8 @@
 <?php
 
-$target_dir = "uploads/";
+// $file_uploads = on;
+
+$target_dir = "/Users/845836268/Desktop/dataVis/www/4-April/jesseWalsh-PhpPhotoUpload/uploads";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
@@ -18,41 +20,45 @@ if(isset($_POST["submit"])) {
     }
 }
 
+
+
 /* Adding some restrictions:
 
 Check if file already exists
 
 Check if the file already exists in the "uploads" folder. If it does, an error message is displayed, and $uploadOk is set to 0: */
 
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
+// if (file_exists($target_file)) {
+//     echo "Sorry, file already exists.";
+//     $uploadOk = 0;
+// }
 
 /*  Check file size
 
 If the file is larger than 500KB, an error message is displayed, and $uploadOk is set to 0: */
 
-if ($_FILES["fileToUpload"]["size"] > 50000000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
+// if ($_FILES["fileToUpload"]["size"] > 50000000) {
+//     echo "Sorry, your file is too large.";
+//     $uploadOk = 0;
+// }
 
 /* Allow certain file formats
 
 The code below only allows users to upload JPG, JPEG, PNG, and GIF files. All other file types gives an error message before setting $uploadOk to 0: */
 
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-	echo "<br>";
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
+// if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+// && $imageFileType != "gif" ) {
+// 	echo "<br>";
+//     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+//     $uploadOk = 0;
+// }
 
 // Check if $uploadOk is set to 0 by an error
+
 if ($uploadOk == 0) {
 	echo "<br>";
     echo "Sorry, your file was not uploaded.";
+
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
